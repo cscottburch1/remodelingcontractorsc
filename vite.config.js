ce0513524f61b283f9f76daa82dfab241317d32a
cscottburch1 <scott.burchcontracting@gmail.com>
2026-04-12 00:11:00 -0400

    Split vendor chunks and remove NODE_ENV from env file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2020',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'helmet': ['react-helmet-async'],
+        },
+      },
+    },
+  },
   server: {
     port: 3002,
     strictPort: true,
