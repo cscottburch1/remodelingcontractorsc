203609d75e505188f9532257817769ca4d0e2948
cscottburch1 <scott.burchcontracting@gmail.com>
2026-04-28 22:45:07 -0400

    fix: force add missing businessProfile.js (ignored by gitignore)

---
+++ src/data/businessProfile.js
@@ -0,0 +1,36 @@
+export const BUSINESS_PROFILE = {
+  businessName: 'Remodeling Contractors SC',
+  siteUrl: 'https://remodelingcontractorsc.com',
+  phone: '+1-864-724-4600',
+  email: 'estimates@remodelingcontractorsc.com',
+  yearsExperienceLabel: '35+ Years Experience',
+  credentials: {
+    googleRatingLabel: '5.0 Google Rating',
+    bbbLabel: 'BBB A+',
+    licenseLabel: 'SC Licensed',
+  },
+  address: {
+    streetAddress: '30 Patewood Drive',
+    addressLocality: 'Greenville',
+    addressRegion: 'SC',
+    postalCode: '29615',
+    addressCountry: 'US',
+  },
+  aggregateRating: {
+    ratingValue: '5.0',
+    reviewCount: '12',
+    bestRating: '5',
+    worstRating: '1',
+  },
+  serviceArea: [
+    'Greenville SC',
+    'Simpsonville SC',
+    'Mauldin SC',
+    'Fountain Inn SC',
+    'Greer SC',
+    'Taylors SC',
+    'Travelers Rest SC',
+    'Piedmont SC',
+    'Easley SC',
+  ],
+};
